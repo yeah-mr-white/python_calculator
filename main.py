e4f85697b1c877e589030da850aaacfcebc3ce19
yeah-mr-white <eyeforparrot78145@gmail.com>
2025-05-24 01:14:17 +0700

    completed CLI version of calculator

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('hello, world')
+first_num = float(input('Enter first num: '))
+second_num = float(input('Enter second num: '))
+operator = input('Enter operator: ')
+
+def calculation(operator):
+    match operator:
+        case '+':
+            return first_num + second_num
+        case '-':
+            return first_num - second_num
+        case '*':
+            return first_num * second_num
+        case '/':
+            return first_num / second_num
+
+try:
+    result = calculation(operator)
+    print(result)
+except ZeroDivisionError:
+    print(f"Can't divide by zero.")
